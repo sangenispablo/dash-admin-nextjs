420aebd78604f0a388f28b1c68a06233ce4b00db
Pablo Alejandro Sangenis <pasangenis@teco.com.ar>
2024-06-14 16:00:32 -0300

    Loading y streaming en page complete

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,14 @@
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
-import { lusitana } from "../ui/fonts";
-import RevenueChart from "../ui/dasboard/revenue-chart";
-import LatestInvoices from "../ui/dasboard/latest-invoices";
-import CardWrapper from "../ui/dasboard/cards";
+import { lusitana } from "../../ui/fonts";
+
+import { fetchRevenue, fetchLatestInvoices } from "../../lib/data";
+
+import RevenueChart from "../../ui/dasboard/revenue-chart";
+import LatestInvoices from "../../ui/dasboard/latest-invoices";
+import CardWrapper from "../../ui/dasboard/cards";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();  
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
